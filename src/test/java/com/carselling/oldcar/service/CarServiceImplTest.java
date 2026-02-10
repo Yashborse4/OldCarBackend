0951e758336ceaad36ca458b34efba211b82de76
yash borse <yashborse432005@gmail.com>
2026-02-10 20:56:16 +0530

    chore: Update tests and remove lint logs

--- src/test/java/com/carselling/oldcar/service/CarServiceImplTest.java
+++ src/test/java/com/carselling/oldcar/service/CarServiceImplTest.java
@@ -28,7 +28,6 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.never;
